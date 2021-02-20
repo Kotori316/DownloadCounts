535a26cf71b99b1b3f6c0436eca7b0a7855ef4ac
Kotori316 <Kotori316@gmail.com>
2021-02-20 21:17:15 +0900

    format

--- src/main/java/com/kotori316/dl/Main2.java
+++ src/main/java/com/kotori316/dl/Main2.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,9 +15,22 @@ import java.util.regex.Pattern;
 import com.therandomlabs.curseapi.CurseAPI;
 import com.therandomlabs.curseapi.CurseException;
 
+import static java.time.temporal.ChronoField.*;
+
 public class Main2 {
     private static final Pattern CSV_ENTRY = Pattern.compile("(.+),(\\d+),(\\d+)");
-    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM");
+    private static final DateTimeFormatter YEAR_MONTH = DateTimeFormatter.ofPattern("yyyy-MM");
+    private static final DateTimeFormatter LOCAL_DATE = new DateTimeFormatterBuilder()
+        .parseCaseInsensitive()
+        .append(DateTimeFormatter.ISO_LOCAL_DATE)
+        .appendLiteral('T')
+        .appendValue(HOUR_OF_DAY, 2)
+        .appendLiteral(':')
+        .appendValue(MINUTE_OF_HOUR, 2)
+        .optionalStart()
+        .appendLiteral(':')
+        .appendValue(SECOND_OF_MINUTE, 2)
+        .toFormatter();
 
     public static void main(String[] args) throws CurseException {
         var time = ZonedDateTime.now();
@@ -37,7 +51,7 @@ public class Main2 {
                     }
                     try (var writer = Files.newBufferedWriter(csvPath, StandardOpenOption.APPEND)) {
                         writer.newLine();
-                        writer.write(String.format("%s,%d,%d", time.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), count - lastDownload, count));
+                        writer.write(String.format("%s,%d,%d", time.format(LOCAL_DATE), count - lastDownload, count));
                     }
                 } catch (IOException e) {
                     throw new UncheckedIOException(e);
@@ -48,7 +62,7 @@ public class Main2 {
     }
 
     static Path createCsv(String projectName, ZonedDateTime time) {
-        Path csvPath = Path.of(projectName, time.format(format) + ".csv");
+        Path csvPath = Path.of(projectName, time.format(YEAR_MONTH) + ".csv");
         if (Files.notExists(csvPath)) {
             try {
                 if (Files.notExists(csvPath.getParent())) {
